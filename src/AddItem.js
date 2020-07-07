700f131f15c6a34847a278a9baef4a72eca5084c
Yewei Li <38413224+lywme@users.noreply.github.com>
2020-07-07 16:41:55 -0400

    using material UI

---
+++ src/AddItem.js
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import Skylight from 'react-skylight';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+class AddItem extends Component{
+
+    constructor(props)
+    {
+        super(props)
+        this.state={product:'',amount:''}
+    }
+
+    additem=()=>{
+        const item={product:this.state.product,amount:this.state.amount};
+        this.props.additem(item);
+        this.setState({product:'',amount:''});
+        this.addform.hide();
+    };
+
+    change=(e)=>{
+        this.setState({[e.target.name]:e.target.value});
+    };
+
+    render(){
+        return (
+        <div>
+            <Button variant="contained" color="primary" onClick={()=>this.addform.show()}>Add</Button>
+        <Skylight
+            hideOnOverlayClicked
+            ref={ref=>this.addform=ref}
+            title="add item" 
+        >
+        
+            <TextField type='text' onChange={this.change} value={this.state.product} name='product' placeholder='product' />
+            <br />
+            <TextField type='text' onChange={this.change} value={this.state.amount} name='amount' placeholder='amount' />
+            <br />
+            <Button variant='contained' onClick={this.additem}>Add to cart</Button>
+        
+        </Skylight>
+        </div>
+        );
+    }
+}
+
+
+export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Additem from './AddItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.state={items:[]}
+  }
+
+  additem=(item)=>{
+    this.setState({items:[item,...this.state.items]});
+  };
+
+  render()
+  {
+    const cartlist=this.state.items.map((each,index)=>
+    <li key={index}>{each.product}  {each.amount}</li>
+    );
+
+    return (
+      <div className="App">
+        <Additem additem={this.additem} />
+        {cartlist}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
